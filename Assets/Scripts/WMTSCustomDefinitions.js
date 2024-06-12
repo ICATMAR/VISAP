c1070e2e60f6ef175446c75f090f00824568c2b0
Gerard Llorach <gerard.llorach.work@gmail.com>
2024-06-12 11:29:02 +0200

    added styles for other WMTS layers

--- Assets/Scripts/WMTSCustomDefinitions.js
+++ Assets/Scripts/WMTSCustomDefinitions.js
@@ -5,11 +5,21 @@ const customDefinitions = {
     shortName: 'Wave height',
     altNames: ['Hs', 'Hm0', 'Wave significant height', 'Spectral significant wave height (Hm0)'],
     range: [0,12],
+    unit: 'm',
     animation: {
       layerNames: ['VHM0', 'VMDR'], // Intensity, Angle
       format: 'value_angle',
       type: 'wave'
     },
+    // Styles
+    // Legends from Assets/Legends
+    legends: ['Occam', 'Alg2', 'Inferno', 'OccamPastel', 'Zebra'],
+    legendRanges: [
+      [0, 4],
+      [0, 8],
+      [0, 12],
+      [0, 3],
+    ],
   },
   'VTM02': {
     shortName: 'Wave period',
@@ -31,6 +41,7 @@ const customDefinitions = {
     shortName: 'Wind wave height',
     altNames: ['Wind wave significant height', "Spectral significant wind wave height", 'Wind waves', 'WWSH'],
     range: [0,12],
+    unit: 'm',
     animation: {
       layerNames: ['VHM0_WW', 'VMDR_WW'], // Intensity, Angle
       format: 'value_angle',
@@ -47,6 +58,7 @@ const customDefinitions = {
   'VHM0_SW1': { // Swell 1 wave height
     range: [0, 12],
     altNames: ["Primary swell wave significant height"],
+    unit: 'm',
     animation: {
       layerNames: ['VHM0_SW1', 'VMDR_SW1'], // Intensity, Angle
       format: 'value_angle',
@@ -56,6 +68,7 @@ const customDefinitions = {
   'VHM0_SW2': { // Swell 2 wave height
     range: [0, 12],
     altNames: ["Secondary swell wave significant height"],
+    unit: 'm',
     animation: {
       layerNames: ['VHM0_SW2', 'VMDR_SW2'], // Intensity, Angle
       format: 'value_angle',
@@ -93,23 +106,54 @@ const customDefinitions = {
     range: [2, 38],
     shortName: 'Bottom temperature',
     altNames: ['Sea bottom temperature'],
-    unit: 'ºC'
+    unit: 'ºC',
+    // Styles
+    // Legends from Assets/Legends
+    legends: ['Occam', 'Alg2', 'Inferno', 'OccamPastel', 'Zebra'],
+    legendRanges: [
+      [10, 16],
+      [5, 25],
+    ],
   },
   'sst_anomaly': {
     range: [-8, 8],
     shortName: 'Sea surface temperature anomaly',
     altNames: ['Sea temperature anomaly', 'Temperature anomaly'],
-    unit: 'ºC'
+    unit: 'ºC',
+    // Styles
+    // Legends from Assets/Legends
+    legends: ['TwoSidedOccam','TwoSidedBlueWhiteRed', 'TwoSidedDarkScaleColors'],
+    legendRanges: [
+      [-5, 5],
+      [-8, 8],
+      [-2, 2],
+    ],
   },
   'so': {
     shortName: 'Salinity',
     range: [32, 41],
     unit: '‰',
+    // Styles
+    // Legends from Assets/Legends
+    legends: ['OccamCold','Occam', 'Alg2', 'Inferno', 'OccamPastel', 'Zebra'],
+    legendRanges: [
+      [35, 39],
+      [36, 38],
+      [32, 41],
+    ],
   },
   'chl': {
     shortName: 'Chlorophyll',
-    range: [0.01, 0.3],
+    range: [0.01, 0.4],
     unit: 'mg/m³',
+    // Styles
+    // Legends from Assets/Legends
+    legends: ['Green','Occam', 'Alg2', 'Inferno', 'OccamPastel', 'Zebra'],
+    legendRanges: [
+      [0.01, 0.4],
+      [0.01, 0.1],
+      [0.001, 0.6],
+    ],
   }
 }
 
