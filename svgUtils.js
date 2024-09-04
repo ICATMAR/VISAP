dc22b9fef309b7ce60dafac12ecb7cb75d3a6bf6
Gerard Llorach <gerard.llorach.work@gmail.com>
2024-09-04 15:32:31 +0200

    adding axis and labels

---
+++ svgUtils.js
@@ -0,0 +1,210 @@
+// Generate SVG path
+const generateSVGPath = function (sizes, rangeSize, rangeNumInd) {
+    let path = '';
+    rangeSize = [...rangeSize]; // Copy
+    rangeNumInd = [...rangeNumInd]; // Copy
+    rangeSize[1] *= 1.1;
+    rangeNumInd[1] *= 1.1;
+
+    let sizesKeys = Object.keys(sizes);
+    // If there is no data
+    if (sizesKeys[1] == undefined) {
+        let xPos = sizesKeys[0] / rangeSize[1];
+        return 'M ' + (xPos - 0.01) + ' 0 L ' + xPos + ' ' + sizes[sizesKeys[0]].numInd / rangeNumInd[1] + ' L ' + (xPos + 0.01) + ' 0'; // One peak
+    }
+    // First two points
+    let step = sizesKeys[0] - (sizesKeys[1] - sizesKeys[0]);
+    if (isNaN(step / rangeSize[1])) { debugger; }
+    path += 'M 0 0 L ' + step / rangeSize[1] + ' 0 ';
+    // Data points
+    for (const sKey of sizesKeys) {
+        path += 'L ' + sKey / rangeSize[1] + ' ' + sizes[sKey].numInd / rangeNumInd[1] + ' ';
+        if (sizes[sKey].numInd / rangeNumInd[1] > 1) { debugger }
+    }
+    // End point
+    path += 'L 1 0';
+
+    return path;
+}
+
+
+
+
+
+// Create HTML plot element
+const createPlotHTMLEl = (svgPath, title, xlabel, ylabel, color) => {
+    let plotEl = document.createElement('div');
+    plotEl.classList.add('plot-container');
+    // First row
+    // ylabel-yaxis-plot
+    let topRowEl = document.createElement('div');
+    topRowEl.classList.add('ylabel-yaxis-plot-container');
+    // Add elements
+    // ylabel
+    let ylabelEl = document.createElement('div');
+    ylabelEl.classList.add('ylabel');
+    let ylabelTextEl = document.createElement('div');
+    ylabelTextEl.classList.add('ylabel-text');
+    ylabelTextEl.innerText = ylabel;
+    ylabelEl.appendChild(ylabelTextEl);
+    // yaxis
+    let yaxisEl = document.createElement('div');
+    yaxisEl.classList.add('yaxis');
+    // svg
+    let svgEl = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+    svgEl.classList.add('plot');
+    svgEl.setAttribute('viewBox', '0 0 1 1');
+    svgEl.setAttribute('preserveAspectRatio', 'none');
+    // path
+    let pathEl = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+    pathEl.setAttribute('d', svgPath);
+    pathEl.setAttribute('stroke-linejoin', 'round');
+    pathEl.setAttribute('vector-effect', 'non-scaling-stroke');
+    pathEl.classList.add('path');
+    pathEl.style.stroke = color;
+    pathEl.style.fill = color.replace('0.85)', '0.4)');
+    svgEl.appendChild(pathEl);
+    topRowEl.append(ylabelEl, yaxisEl, svgEl);
+    plotEl.appendChild(topRowEl);
+    // Second row
+    // xaxis-container
+    let xaxisRowEl = document.createElement('div');
+    xaxisRowEl.classList.add('xaxis-container');
+    let fillerEl = document.createElement('div');
+    fillerEl.classList.add('ylabel');
+    let fillerEl2 = document.createElement('div');
+    fillerEl2.classList.add('yaxis');
+    xaxisRowEl.append(fillerEl, fillerEl2);
+    // xaxis
+    let xaxisEl = document.createElement('div');
+    xaxisEl.classList.add('xaxis');
+    xaxisRowEl.append(xaxisEl);
+    // right: 0, width: same as svg
+    plotEl.appendChild(xaxisRowEl);
+    // Third row
+    // xlabel
+    let xlabelEl = document.createElement('div');
+    xlabelEl.classList.add('xlabel');
+    xlabelEl.innerText = xlabel;
+    plotEl.appendChild(xlabelEl);
+    // right: 0, width: same as svg
+    // Fourth row
+    // title
+    let titleEl = document.createElement('div');
+    titleEl.classList.add('title')
+    titleEl.innerText = title;
+    plotEl.appendChild(titleEl);
+
+    return plotEl;
+}
+
+
+const createMultiplePlotHTMLEl = (specData, keyClassName, title, xlabel, ylabel, color) => {
+    let plotEl = document.createElement('div');
+    plotEl.classList.add('plot-container');
+    // First row
+    // ylabel-yaxis-plot
+    let topRowEl = document.createElement('div');
+    topRowEl.classList.add('ylabel-yaxis-plot-container');
+    // Add elements
+    // ylabel
+    let ylabelEl = document.createElement('div');
+    ylabelEl.classList.add('ylabel');
+    let ylabelTextEl = document.createElement('div');
+    ylabelTextEl.classList.add('ylabel-text');
+    ylabelTextEl.innerText = ylabel;
+    ylabelEl.appendChild(ylabelTextEl);
+    // yaxis
+    let yaxisEl = document.createElement('div');
+    yaxisEl.classList.add('yaxis');
+
+
+    // svg
+    let svgEl = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+    svgEl.classList.add('plot');
+    svgEl.setAttribute('viewBox', '0 0 1 1');
+    svgEl.setAttribute('preserveAspectRatio', 'none');
+
+    let numSubPlots = Object.keys(specData[keyClassName]).length;
+    Object.keys(specData[keyClassName]).forEach((key, index) => {
+        // Create g to wrap the paths
+        let gEl = document.createElementNS('http://www.w3.org/2000/svg', 'g');
+        // path
+        let pathEl = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+        pathEl.setAttribute('d', specData[keyClassName][key].svgPath);
+        pathEl.setAttribute('stroke-linejoin', 'round');
+        pathEl.setAttribute('vector-effect', 'non-scaling-stroke');
+        pathEl.classList.add('path');
+        pathEl.style.stroke = color;
+        pathEl.style.fill = color.replace('0.85)', '0.4)');
+        gEl.appendChild(pathEl);
+
+        gEl.setAttribute('transform', 'translate(0, -' + index / (numSubPlots + 2) + ')');
+        svgEl.appendChild(gEl);
+    });
+
+    topRowEl.append(ylabelEl, yaxisEl, svgEl);
+    plotEl.appendChild(topRowEl);
+
+
+
+    // Second row
+    // xaxis-container
+    let xaxisRowEl = document.createElement('div');
+    xaxisRowEl.classList.add('xaxis-container');
+    let fillerEl = document.createElement('div');
+    fillerEl.classList.add('ylabel');
+    let fillerEl2 = document.createElement('div');
+    fillerEl2.classList.add('yaxis');
+    xaxisRowEl.append(fillerEl, fillerEl2);
+    // xaxis
+    let xaxisEl = document.createElement('div');
+    xaxisEl.classList.add('xaxis');
+    xaxisRowEl.append(xaxisEl);
+    // right: 0, width: same as svg
+    plotEl.appendChild(xaxisRowEl);
+    // Third row
+    // xlabel
+    let xlabelEl = document.createElement('div');
+    xlabelEl.classList.add('xlabel');
+    xlabelEl.innerText = xlabel;
+    plotEl.appendChild(xlabelEl);
+    // right: 0, width: same as svg
+    // Fourth row
+    // title
+    let titleEl = document.createElement('div');
+    titleEl.classList.add('title')
+    titleEl.innerText = title;
+    plotEl.appendChild(titleEl);
+
+    return plotEl;
+}
+
+
+// Create x axis tips
+const createXAxisTips = (maxValue, width, step) => {
+    // Pixel separation between ticks
+    let pixelSeparation = 100;
+    // Number of ticks
+    let maxNumTicks = (width / pixelSeparation);
+    // Number of possible steps
+    let maxNumSteps = (maxValue / step);
+    // Skip step when iterating steps
+    let skipStep = Math.ceil(maxNumSteps / maxNumTicks);
+
+    // Create g to wrap the xticks
+    let gEl = document.createElementNS('http://www.w3.org/2000/svg', 'g');
+    for (let i = 0; i < Math.floor(maxNumSteps); i += skipStep) {
+        let normX = i / maxNumSteps;
+        // path
+        let pathEl = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+        pathEl.setAttribute('d', 'M ' + normX + ' 0 L ' + normX + ' 0.1');
+        pathEl.setAttribute('stroke-linejoin', 'round');
+        pathEl.setAttribute('vector-effect', 'non-scaling-stroke');
+        pathEl.classList.add('path');
+        pathEl.style.stroke = 'black';
+        pathEl.style.fill = 'none'
+        gEl.appendChild(pathEl);
+    }
+    return gEl;
+}
