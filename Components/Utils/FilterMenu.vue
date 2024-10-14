2f2d23398bc06f38b33e161e463d6035786738c9
Gerard Llorach <gerard.llorach@gmail.com>
2024-10-14 16:58:37 +0200

    setting back filter menu as it was before length distribution implementation, so it works for catch composition

--- Components/Utils/FilterMenu.vue
+++ Components/Utils/FilterMenu.vue
@@ -2,38 +2,36 @@
   <!-- Container -->
   <div id='filterMenu' ref='filterMenu'>
 
-    <!-- Target species -->
-    <div class="target-container">
-      <!-- Species -->
-      <button v-for="sp in targetSpecies" :class="[sp.name == selSpecies ? 'button-active' : '']" @click="clickedSpecies(sp)">
-        <span :style="{color: 'rgb(' + sp.color +')'}" > ■ </span> {{$t(sp.name)}} ({{sp.name}})
-      </button>
+    <!-- Wrapper -->
+    <div class="wrapper">
 
-      <!-- Species not target but selected -->
-      <Transition>
-        <button :class="[selButNotTarget.name == selSpecies ? 'button-active' : '']" @click="clickedSpecies(selButNotTarget)" v-show="selButNotTarget.isVisible">
-          <span :style="{color: 'rgb(' + selButNotTarget.color +')'}" > ■ </span> {{$t(selButNotTarget.name)}} ({{selButNotTarget.name}})
-        </button>
-      </Transition>
-    </div>
-
-    <!-- More species -->
-    <button style="border: 1px solid" @click="isAllSpeciesMenuVisible = true">+ {{ $t('More species') }}</button>
-
-    <div class="overlay" @click="isAllSpeciesMenuVisible = false" v-show="isAllSpeciesMenuVisible">
-      <!-- Wrapper -->
-      <div class="wrapper">
-
-        <!-- Species list -->
-        <div ref="availableSpecies" class="listSpeciesContainer">
-          <div class="search-bar-container">
-            <input type="search" class="search form-control" onclick="event.stopPropagation();" :placeholder="$t('Search')" />
-            <button class="icon-str button-active clickable">x</button>
-          </div>
-          <div class="list"></div>
-        </div>
-
+      <!-- User buttons -->
+      <div class="center-buttons" ref="controlButtons">
+        <button ref="selectAll" onclick="event.stopPropagation();">&#x25C6; {{$t('Select all')}} </button>
+        <button ref="deselectAll" onclick="event.stopPropagation();">&#x25C7; {{$t('Deselect all')}} </button>
+        <button ref="closeGUIApply" onclick="event.stopPropagation();"><span class="fa">&#xf0b0;</span> {{$t('Apply filter')}} </button>
+        <button ref="closeGUIClear" onclick="event.stopPropagation();"><span class="fa">&#xe17b;</span> {{$t('Clear filter')}} </button>
       </div>
+
+      <!-- Selected species -->
+      <div ref="selSpecies" class="listSpeciesContainer selSpeciesContainer">
+        <input type="search" class="search form-control" onclick="event.stopPropagation();" :placeholder="$t('Search')" v-show="showSearchBarSelSpecies"/>
+        <div class="list listSel"></div>
+      </div>
+
+      <!-- Species list -->
+      <div ref="availableSpecies" class="listSpeciesContainer">
+        <input type="search" class="search form-control" onclick="event.stopPropagation();" :placeholder="$t('Search')" />
+        <div class="list"></div>
+      </div>
+
+
+    <!-- Species -->
+    <!-- <button v-for="sp in species">
+      <span :style="{color: 'rgb(' + sp.color +')'}" > ■ </span> {{sp.name}}
+    </button> -->
+
+
     </div>
 
   </div>
@@ -57,37 +55,46 @@ export default {
   data (){
     return {
       showSearchBarSelSpecies: false,
-      isAllSpeciesMenuVisible: false,
-      targetSpecies: [],
-      species: [],
-      selSpecies: '',
-      selButNotTarget: {
-        name: '',
-        color: [127, 127, 127],
-        isVisible: false,
-      }
     }
   },
   methods: {
     // USER INTERACTION
-    // Clicked on species
-    clickedSpecies: function(sp){
-      this.selSpecies = sp.name;
-      window.eventBus.emit('FilterMenu_SelectedSpecies', sp.name);
+
+    // Select item
+    selectItem: function(e){
+      e.stopPropagation();
+      //console.log(e.srcElement.innerText.split("■ ")[1]);
+      let speciesName = this.extractSpeciesName(e.currentTarget.innerText);
+      this.switchFromList(speciesName, this.speciesList, this.selSpeciesList, this.deselectItem);
     },
 
-    clickedSpeciesFromOverlay: function(e){
+    // Deselect item
+    deselectItem: function(e){
       e.stopPropagation();
       let speciesName = this.extractSpeciesName(e.currentTarget.innerText);
-      this.selButNotTarget.name = speciesName;
-      this.selButNotTarget.isVisible = true;
-      this.selButNotTarget.color = palette[speciesName] != undefined ? palette[speciesName].color : [127, 127, 127];
-      // Close GUI
-      this.isAllSpeciesMenuVisible = false;
-      // Trigger click event
-      this.clickedSpecies(this.selButNotTarget);
+      this.switchFromList(speciesName, this.selSpeciesList, this.speciesList, this.selectItem);
     },
 
+    selectAll: function(e){
+      e.stopPropagation();
+      for (var i = 0; i<this.speciesList.items.length; i++){
+        this.switchFromList(this.speciesList.items[i]._values.name, this.speciesList, this.selSpeciesList, this.deselectItem)
+        i--;
+      }
+    },
+
+    deselectAll: function(e){
+      e.stopPropagation();
+      for (var i = 0; i<this.selSpeciesList.items.length; i++){
+        this.switchFromList(this.selSpeciesList.items[i]._values.name, this.selSpeciesList, this.speciesList, this.selectItem)
+        i--;
+      }
+    },
+
+    // Close filter menu
+    closeGUI: function(e){
+      this.$emit('onclose', this.selSpeciesList.toJSON());
+    },
 
     // Separate icon from species name
     extractSpeciesName: function(text){
@@ -99,28 +106,23 @@ export default {
 
 
     // PUBLIC METHODS
+    //onclick: function(e){},
     setData: function(data){
-      let species = Object.keys(data);
+      let species = this.getUnique(data, "ScientificName");
+      if (species.length == 0) // Legacy
+        species = this.getUnique(data, "NomEspecie");
+      
+      let spObj = [];
+      let selSpObj = [];
       // Order alphabethically
       species.sort();
-
-      let spObj = [];
-      this.targetSpecies = [];
-      
       // Get color
       species.forEach((sp, i) => {
-        let el = {
+        spObj.push({
           'name': sp,
           'commonName': this.$i18n.t(sp), // TODO: check if this is made every time the filter is cliked. maybe yes?
           'color': palette[sp] != undefined ? palette[sp].color : [127, 127, 127],
-        }
-        let isTarget = data[sp].rawData[0].TargetSpecies;
-        if (isTarget) {
-          this.targetSpecies.push(el);
-        }
-        else {
-          spObj.push(el);
-        }
+        });
       });
 
       // https://listjs.com/api/
@@ -131,30 +133,87 @@ export default {
           </button>
           `
       }
+      let optionsSel = {
+        item: (sp) =>
+          `<button class="selSpeciesItem" title="${sp.commonName}">
+            <span style='color:red'> ✖ </span>
+            <span style="color: rgb(${sp.color.toString()})" > ■ </span> ${sp.name}
+          </button>
+          `
+      }
 
       // If list exists, reindex as HTML changed
       if (this.speciesList != undefined) {
         // Update list
         this.speciesList.clear();
+        this.speciesList.update();
+        this.selSpeciesList.clear();
+        this.selSpeciesList.update();
       }
 
-      // // Create list      
+      // Create list      
       this.speciesList = new List(this.$refs.availableSpecies, options, spObj);
+      this.selSpeciesList = new List(this.$refs.selSpecies, optionsSel);
 
-      // // Add button events
-      this.speciesList.list.childNodes.forEach((el)=>el.addEventListener("click", (e)=>this.clickedSpeciesFromOverlay(e)));
+      // Add button events
+      this.speciesList.list.childNodes.forEach((el)=>el.addEventListener("click", (e)=>this.selectItem(e)));
+      this.selSpeciesList.list.childNodes.forEach((el)=>el.addEventListener("click", (e)=> this.deselectItem(e)));
+      this.$refs.selectAll.addEventListener("click", (e)=>this.selectAll(e));
+      this.$refs.deselectAll.addEventListener("click", (e)=>this.deselectAll(e));
+      this.$refs.closeGUIApply.addEventListener("click", (e) => this.closeGUI(e));
+      this.$refs.closeGUIClear.addEventListener("click", (e) => {this.deselectAll(e); this.closeGUI(e)});
   
     },
 
 
-    selectRandomTargetSpecies(){
-      let randIndex = Math.floor(Math.random()*this.targetSpecies.length);
-      this.selSpecies = this.targetSpecies[randIndex].name;
-      window.eventBus.emit('FilterMenu_SelectedSpecies', this.targetSpecies[randIndex].name);
+    // Set selected
+    addSelected(speciesName){
+      this.switchFromList(speciesName, this.speciesList, this.selSpeciesList, this.deselectItem);
     },
 
 
 
+
+
+
+
+    // INTERNAL METHODS
+    // Switch values from one list to the other
+    switchFromList(speciesName, removeFromList, insertToList, callbackFuncBtn){
+      let item = removeFromList.get("name", speciesName)[0];
+      removeFromList.remove("name", speciesName);
+
+      let itNew = insertToList.add({
+        "name": speciesName,
+        'commonName': this.$i18n.t(speciesName), // TODO: check if this is made every time the filter is cliked. maybe yes?
+        "color":  item._values.color
+      });
+      // Add event listener
+      callbackFuncBtn = callbackFuncBtn.bind(this); // bind callback with this
+      itNew[0].elm.addEventListener("click", (e)=>callbackFuncBtn(e));
+
+      // Turn on/off the search bar for selected species
+      this.showSearchBarSelSpecies = this.selSpeciesList.size() > 20 ? true : false;
+      
+    },
+
+
+
+
+
+
+
+    // Get unique values in an array of objects
+    getUnique: function(data, key){
+      let uniqueKeys = [];
+      // Iterate
+      for (let i = 0; i < data.length; i++){
+        let value = data[i][key];
+        if (value !== undefined && uniqueKeys.findIndex((item) => item == value) == -1)
+          uniqueKeys.push(value);
+      }
+      return uniqueKeys;
+    }
   },
   components: {
     //'map': Map,
@@ -167,26 +226,6 @@ export default {
 
 <style scoped>
 #filterMenu {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
-
-.target-container {
-  display:flex;
-  flex-direction: row;
-  justify-content: center;
-  padding: 20px;
-  flex-wrap: wrap;
-  font-size: small;
-}
-
-.target-container > button {
-  margin: 2px;
-  border: solid 1px;
-}
-
-.overlay {
   position: absolute;
   width: 100vw;
   height: 100vh;
@@ -205,7 +244,12 @@ export default {
   height: 100%;
 }
 
-
+.center-buttons {
+  display:flex;
+  flex-direction: row;
+  justify-content: center;
+  padding: 20px;
+}
 
 .listSpeciesContainer {
   display: flex;
@@ -218,21 +262,18 @@ export default {
   padding: 20px;
 }
 
-
-
-
-.search-bar-container{
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  width: 100%;
-  justify-content: center;
-  margin-top: 20px;
+.selSpeciesContainer {
+  background-color: var(--red);
+  flex: 0 0 auto;
+  padding: 20px;
 }
 
 input {
   width: 50%;
+  margin-top: 20px;
 }
+
+
 
 .list {
   background: rgba(255, 255, 255, 0.247);
@@ -248,6 +289,10 @@ input {
   justify-content: center;
 }
 
+.listSel {
+  background-color: var(--red);
+  max-height: 30vh;
+}
 
 .speciesItem {
   margin: 2px;
